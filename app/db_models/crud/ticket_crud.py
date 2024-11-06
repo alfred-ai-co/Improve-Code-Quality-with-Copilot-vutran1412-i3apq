30c674c38fc093849b72a3dc29449a8b208acf9e
Vu Tran <vutran1412@icloud.com>
2024-11-05 19:31:54 -0600

    Add limit to ticket crud

--- app/db_models/crud/ticket_crud.py
+++ app/db_models/crud/ticket_crud.py
@@ -2,12 +2,13 @@ from sqlalchemy.orm import Session
 from sqlalchemy import select
 from app.db_models.crud.base_crud import BaseCRUD
 from app.db_models.base import Ticket
+from typing import List
 
 class TicketCRUD(BaseCRUD):
     def __init__(self, db: Session):
         super().__init__(db, Ticket)
 
-    def get_all(self, skip: int = 0, limit: int = 10):
+    def get_all(self, skip: int = 0, limit: int = 10) -> List[Ticket]:
         result = self.db.execute(
             select(Ticket).offset(skip).limit(limit)
         )
