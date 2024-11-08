1564fcfde6e50c31a61f4f349d0b3f016fd1c321
Vu Tran <vutran1412@icloud.com>
2024-11-08 11:37:20 -0600

    Added exception handling and documentation in the README

--- app/api/routes/kanbanstatus.py
+++ app/api/routes/kanbanstatus.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException
 from sqlalchemy.orm import Session
 from fastapi import Depends
+from sqlalchemy.exc import SQLAlchemyError
 
 from app.db_models.crud import KanbanStatusCRUD
 from app.api_models.kanbanstatus import KanbanStatusCreate, KanbanStatusResponse
@@ -12,34 +13,49 @@ router = APIRouter()
 
 @router.post("/", status_code=201, response_model=KanbanStatusResponse)
 def create_kanban_status(kanban_status: KanbanStatusCreate, db: Session = Depends(get_db)):
-    kanban_status_crud = KanbanStatusCRUD(db)
-    return kanban_status_crud.create(**kanban_status.model_dump())
+    try:
+        kanban_status_crud = KanbanStatusCRUD(db)
+        return kanban_status_crud.create(**kanban_status.model_dump())
+    except SQLAlchemyError as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 @router.get("/", status_code=200, response_model=list[KanbanStatusResponse])
 def get_all_kanban_statuses(db: Session = Depends(get_db)):
-    kanban_status_crud = KanbanStatusCRUD(db)
-    return kanban_status_crud.get_all()
+    try:
+        kanban_status_crud = KanbanStatusCRUD(db)
+        return kanban_status_crud.get_all()
+    except SQLAlchemyError as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 @router.get("/{id}", status_code=200, response_model=KanbanStatusResponse)
 def get_kanban_status(id: int, db: Session = Depends(get_db)):
-    kanban_status_crud = KanbanStatusCRUD(db)
-    kanban_status = kanban_status_crud.get(id)
-    if not kanban_status:
-        raise HTTPException(status_code=404, detail=f"Kanban Status with id {id} not found")
-    return kanban_status
+    try:
+        kanban_status_crud = KanbanStatusCRUD(db)
+        kanban_status = kanban_status_crud.get(id)
+        if not kanban_status:
+            raise HTTPException(status_code=404, detail=f"Kanban Status with id {id} not found")
+        return kanban_status
+    except SQLAlchemyError as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 @router.put("/{id}", status_code=200, response_model=KanbanStatusResponse)
 def update_kanban_status(id: int, kanban_status: KanbanStatusCreate, db: Session = Depends(get_db)):
-    kanban_status_crud = KanbanStatusCRUD(db)
-    kanban_status_crud.update(id, **kanban_status.model_dump())
-    return kanban_status_crud.get(id)
+    try:
+        kanban_status_crud = KanbanStatusCRUD(db)
+        kanban_status_crud.update(id, **kanban_status.model_dump())
+        return kanban_status_crud.get(id)
+    except SQLAlchemyError as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 @router.delete("/{id}", status_code=204)
 def delete_kanban_status(id: int, db: Session = Depends(get_db)):
-    kanban_status_crud = KanbanStatusCRUD(db)
-    kanban_status_crud.delete(id)
-    return {"message": "Kanban Status deleted successfully"}
+    try:
+        kanban_status_crud = KanbanStatusCRUD(db)
+        kanban_status_crud.delete(id)
+        return {"message": "Kanban Status deleted successfully"}
+    except SQLAlchemyError as e:
+        raise HTTPException(status_code=500, detail=str(e))
